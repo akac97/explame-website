907b74828373f1296b86c41c690d6d9a9410e9af
akac97 <142359207+akac97@users.noreply.github.com>
2024-03-26 17:15:08 +0100

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-// main.js
-function copyEmail() {
-    var email = document.getElementById('email').innerText;
-    navigator.clipboard.writeText(email).then(function() {
-        alert('Email copied to clipboard');
-    }).catch(function() {
-        alert('Failed to copy email');
-    });
-}
